8a88c4d3931be89f824410e2dd8dc01380c3c7ee
edwinkarolczyk <edwinkarolczyk@gmail.com>
2025-09-30 09:19:51 +0200

    Remove obsolete hala/serwis modules

---
+++ core/modules_manifest.py
@@ -0,0 +1,15 @@
+"""Definicja listy modułów aplikacji wykorzystywana w testach i narzędziach."""
+
+from __future__ import annotations
+
+modules: list[str] = [
+    "ustawienia",
+    "profile",
+    "panel_glowny",
+    "narzedzia",
+    "magazyn",
+    "zlecenia",
+    "maszyny",
+]
+
+__all__ = ["modules"]
