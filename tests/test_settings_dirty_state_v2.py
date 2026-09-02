10ee19df55bfa961e931ff33ddc4cc587405973e
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-02 11:02:17 +0200

    test: zaktualizuj oczekiwana wersje WM

--- tests/test_settings_dirty_state_v2.py
+++ tests/test_settings_dirty_state_v2.py
@@ -1,4 +1,4 @@
-# version: 1.1
+# version: 1.2
 from pathlib import Path
 import ast
 
@@ -62,4 +62,4 @@ def test_failed_save_keeps_unsaved_snapshot():
 
 def test_patch_version_bumped():
     from __version__ import get_version
-    assert get_version() == "0.3.2"
+    assert get_version() == "0.4.0"
