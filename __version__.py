2b3eb132d1e079d3699289a0c7c1b95d90027578
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-03 07:43:06 +0200

    chore: podnies wersje WM do 0.6.2

--- __version__.py
+++ __version__.py
@@ -1,4 +1,4 @@
-# version: 1.3
+# version: 1.4
 # -*- coding: utf-8 -*-
 """Centralna wersja aplikacji WM.
 
@@ -11,7 +11,7 @@ Zasada SemVer dla WM:
 - MAJOR (X.0.0): zmiany niekompatybilne lub duża przebudowa aplikacji.
 """
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 
 def get_version() -> str:
