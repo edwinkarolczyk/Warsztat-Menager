0a33fe3690d333d833a48a347b0d5ed6e2d095b7
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-09 18:59:58 +0200

    WM: test wersji 0.13.3

--- tests/test_profile_foreman_edit_runtime.py
+++ tests/test_profile_foreman_edit_runtime.py
@@ -18,7 +18,7 @@ from profile_foreman_edit_runtime import _parse_carryover
 
 
 def test_profile_release_is_current():
-    assert __version__ == "0.13.2"
+    assert __version__ == "0.13.3"
 
 
 def test_profile_shift_modes_match_engine_and_add_dialog():
@@ -43,9 +43,6 @@ def test_profile_shift_mode_sync_patches_legacy_users_panel():
 
     shift_mode_runtime._patch_legacy_users_panel()
     assert tuple(legacy_users.SHIFT_MODE_CHOICES.values()) == tuple(shifts_schedule.TRYBY)
-    assert legacy_users._shift_mode_label_from_code("112") == dict(
-        shift_mode_runtime.SHIFT_MODE_OPTIONS
-    )["112"]
 
 
 def test_profile_shift_mode_labels_save_as_codes():
